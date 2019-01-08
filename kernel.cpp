5434751e0a3664c6b3557d120de8d230d83c52d9
bardia karimi <bardiakarimi7@gmail.com>
2019-01-08 13:18:09 +0330

    third video

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,5 @@
 #include "types.h"
+#include "gdt.h"
 void printf(char* str)
 {
     static uint16_t* VideoMemory = (uint16_t*)0xb8000; //a place to show string on screen by graphic card
@@ -23,6 +24,6 @@ extern "C" void callConstructors()
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t/*multiboot_magic*/) //extern for not change name of function
 {
     printf("Hello World");
-
+    GlobalDescriptorTable gdt;
     while(1);
 }
