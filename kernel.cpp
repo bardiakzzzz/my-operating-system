0ab1106b5400ecc8e65092828ce99f77f974c6f7
bardia karimi <bardiakarimi7@gmail.com>
2019-01-08 13:38:30 +0330

    forth video

--- kernel.cpp
+++ kernel.cpp
@@ -3,9 +3,38 @@
 void printf(char* str)
 {
     static uint16_t* VideoMemory = (uint16_t*)0xb8000; //a place to show string on screen by graphic card
+    static uint8_t x=0,y=0;
 
-    for(int i = 0; str[i] != '\0'; ++i)
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i]; // 2 bytes az once to avoid overwriting to high byte
+    for(int i = 0; str[i] != '\0'; ++i){
+      ///  VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i]; // 2 bytes az once to avoid overwriting to high byte
+      switch(str[i])  /// use it because in perivous way we could not use printf more than one time
+        {
+            case '\n':
+                x = 0;
+                y++;
+                break;
+            default:
+                VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
+                x++;
+                break;
+        }
+
+        if(x >= 80)
+        {
+            x = 0;
+            y++;
+        }
+
+        if(y >= 25)
+        {
+            for(y = 0; y < 25; y++)
+                for(x = 0; x < 80; x++)
+                    VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+            x = 0;
+            y = 0;
+        }
+    }
+
 }
 
 
@@ -23,7 +52,8 @@ extern "C" void callConstructors()
 
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t/*multiboot_magic*/) //extern for not change name of function
 {
-    printf("Hello World");
+    printf("Hello World\n");
+    printf("hi\n");
     GlobalDescriptorTable gdt;
     while(1);
 }
--- makefile
+++ makefile
@@ -5,7 +5,7 @@ GCCPARAMS = -m32 -fno-use-cxa-atexit -nostdlib -fno-builtin -fno-rtti -fno-excep
 ASPARAMS = --32
 LDPARAMS = -melf_i386
 
-objects = loader.o gdt.o kernel.o
+objects = loader.o gdt.o port.o kernel.o
 
 
 
@@ -39,3 +39,8 @@ mykernel.iso: mykernel.bin
 
 install: mykernel.bin
 		sudo cp $< /boot/mykernel.bin
+
+
+.PHONY: clean
+clean:
+		rm -f $(objects) mykernel.bin mykernel.iso
