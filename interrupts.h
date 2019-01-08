475806e721251eb6a0588795441740e70d4454ff
bardia karimi <bardiakarimi7@gmail.com>
2019-01-08 17:47:26 +0330

    sixth video

--- interrupts.h
+++ interrupts.h
@@ -6,10 +6,28 @@
     #include "types.h"
     #include "port.h"
 
+
+    class InterruptManager;
+
+    class InterruptHandler
+    {
+    protected:
+        uint8_t InterruptNumber;
+        InterruptManager* interruptManager;
+        InterruptHandler(InterruptManager* interruptManager, uint8_t InterruptNumber);
+        ~InterruptHandler();
+    public:
+        virtual uint32_t HandleInterrupt(uint32_t esp);
+    };
+
+
     class InterruptManager
     {
-        //friend class InterruptHandler;
+        friend class InterruptHandler;
         protected:
+
+            static InterruptManager* ActiveInterruptManager;
+            InterruptHandler* handlers[256];
 
             struct GateDescriptor
             {
@@ -29,11 +47,10 @@
             } __attribute__((packed));
 
             uint16_t hardwareInterruptOffset;
-            //static InterruptManager* ActiveInterruptManager;
             static void SetInterruptDescriptorTableEntry(uint8_t interrupt,
                 uint16_t codeSegmentSelectorOffset, void (*handler)(),
                 uint8_t DescriptorPrivilegeLevel, uint8_t DescriptorType);
-           ///// we have to handle interrupts for each segment so we need several functions
+
 
             static void InterruptIgnore();
 
@@ -77,6 +94,7 @@
             static void HandleException0x13();
 
             static uint32_t HandleInterrupt(uint8_t interrupt, uint32_t esp);
+            uint32_t DoHandleInterrupt(uint8_t interrupt, uint32_t esp);
 
             Port8BitSlow programmableInterruptControllerMasterCommandPort;
             Port8BitSlow programmableInterruptControllerMasterDataPort;
--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "gdt.h"
 #include "interrupts.h"
+#include "keyboard.h"
 void printf(char* str)
 {
     static uint16_t* VideoMemory = (uint16_t*)0xb8000; //a place to show string on screen by graphic card
@@ -57,6 +58,7 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t/*multiboot_
     printf("hi\n");
     GlobalDescriptorTable gdt;
     InterruptManager interrupts(0x20, &gdt);   //IRQ_BASE = 0x20
+    KeyboardDriver keyboard(&interrupts);
     interrupts.Activate();
     while(1);
 }
