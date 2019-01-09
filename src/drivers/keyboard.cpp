e63b8ced3dde724e9eafa4f713143effac6789a3
bardia karimi <bardiakarimi7@gmail.com>
2019-01-09 10:26:40 +0330

    fix keyboard

--- src/drivers/keyboard.cpp
+++ src/drivers/keyboard.cpp
@@ -52,59 +52,63 @@ void KeyboardDriver::Activate()
 uint32_t KeyboardDriver::HandleInterrupt(uint32_t esp)
 {
     uint8_t key = dataport.Read();
-    
+
     if(handler == 0)
         return esp;
-    
+    static bool Shift = false;
+
     if(key < 0x80)
     {
         switch(key)
         {
-            case 0x02: handler->OnKeyDown('1'); break;
-            case 0x03: handler->OnKeyDown('2'); break;
-            case 0x04: handler->OnKeyDown('3'); break;
-            case 0x05: handler->OnKeyDown('4'); break;
-            case 0x06: handler->OnKeyDown('5'); break;
-            case 0x07: handler->OnKeyDown('6'); break;
-            case 0x08: handler->OnKeyDown('7'); break;
-            case 0x09: handler->OnKeyDown('8'); break;
-            case 0x0A: handler->OnKeyDown('9'); break;
-            case 0x0B: handler->OnKeyDown('0'); break;
+          case 0x02: handler->OnKeyDown('1'); break;
+          case 0x03: handler->OnKeyDown('2'); break;
+          case 0x04: handler->OnKeyDown('3'); break;
+          case 0x05: handler->OnKeyDown('4'); break;
+          case 0x06: handler->OnKeyDown('5'); break;
+          case 0x07: handler->OnKeyDown('6'); break;
+          case 0x08: handler->OnKeyDown('7'); break;
+          case 0x09: handler->OnKeyDown('8'); break;
+          case 0x0A: handler->OnKeyDown('9'); break;
+          case 0x0B: handler->OnKeyDown('0'); break;
 
-            case 0x10: handler->OnKeyDown('q'); break;
-            case 0x11: handler->OnKeyDown('w'); break;
-            case 0x12: handler->OnKeyDown('e'); break;
-            case 0x13: handler->OnKeyDown('r'); break;
-            case 0x14: handler->OnKeyDown('t'); break;
-            case 0x15: handler->OnKeyDown('z'); break;
-            case 0x16: handler->OnKeyDown('u'); break;
-            case 0x17: handler->OnKeyDown('i'); break;
-            case 0x18: handler->OnKeyDown('o'); break;
-            case 0x19: handler->OnKeyDown('p'); break;
+         case 0x10: if(Shift) handler->OnKeyDown('Q');else handler->OnKeyDown('q'); break;
+         case 0x11: if(Shift) handler->OnKeyDown('W');else handler->OnKeyDown('w'); break;
+         case 0x12: if(Shift) handler->OnKeyDown('E');else handler->OnKeyDown('e'); break;
+         case 0x13: if(Shift) handler->OnKeyDown('R');else handler->OnKeyDown('r'); break;
+         case 0x14: if(Shift) handler->OnKeyDown('T');else handler->OnKeyDown('t'); break;
+         case 0x15: if(Shift) handler->OnKeyDown('Y');else handler->OnKeyDown('y'); break;
+         case 0x16: if(Shift) handler->OnKeyDown('U');else handler->OnKeyDown('u'); break;
+         case 0x17: if(Shift) handler->OnKeyDown('I');else handler->OnKeyDown('i'); break;
+         case 0x18: if(Shift) handler->OnKeyDown('O');else handler->OnKeyDown('o'); break;
+         case 0x19: if(Shift) handler->OnKeyDown('P');else handler->OnKeyDown('p'); break;
 
-            case 0x1E: handler->OnKeyDown('a'); break;
-            case 0x1F: handler->OnKeyDown('s'); break;
-            case 0x20: handler->OnKeyDown('d'); break;
-            case 0x21: handler->OnKeyDown('f'); break;
-            case 0x22: handler->OnKeyDown('g'); break;
-            case 0x23: handler->OnKeyDown('h'); break;
-            case 0x24: handler->OnKeyDown('j'); break;
-            case 0x25: handler->OnKeyDown('k'); break;
-            case 0x26: handler->OnKeyDown('l'); break;
 
-            case 0x2C: handler->OnKeyDown('y'); break;
-            case 0x2D: handler->OnKeyDown('x'); break;
-            case 0x2E: handler->OnKeyDown('c'); break;
-            case 0x2F: handler->OnKeyDown('v'); break;
-            case 0x30: handler->OnKeyDown('b'); break;
-            case 0x31: handler->OnKeyDown('n'); break;
-            case 0x32: handler->OnKeyDown('m'); break;
-            case 0x33: handler->OnKeyDown(','); break;
-            case 0x34: handler->OnKeyDown('.'); break;
-            case 0x35: handler->OnKeyDown('-'); break;
+         case 0x1E: if(Shift) handler->OnKeyDown('A');else handler->OnKeyDown('a'); break;
+         case 0x1F: if(Shift) handler->OnKeyDown('S');else handler->OnKeyDown('s'); break;
+         case 0x20: if(Shift) handler->OnKeyDown('D');else handler->OnKeyDown('d'); break;
+         case 0x21: if(Shift) handler->OnKeyDown('F');else handler->OnKeyDown('f'); break;
+         case 0x22: if(Shift) handler->OnKeyDown('G');else handler->OnKeyDown('g'); break;
+         case 0x23: if(Shift) handler->OnKeyDown('H');else handler->OnKeyDown('h'); break;
+         case 0x24: if(Shift) handler->OnKeyDown('J');else handler->OnKeyDown('j'); break;
+         case 0x25: if(Shift) handler->OnKeyDown('K');else handler->OnKeyDown('k'); break;
+         case 0x26: if(Shift) handler->OnKeyDown('L');else handler->OnKeyDown('l'); break;
 
-            case 0x1C: handler->OnKeyDown('\n'); break;
-            case 0x39: handler->OnKeyDown(' '); break;
+         case 0x2C: if(Shift) handler->OnKeyDown('Z');else handler->OnKeyDown('z'); break;
+         case 0x2D: if(Shift) handler->OnKeyDown('X');else handler->OnKeyDown('x'); break;
+         case 0x2E: if(Shift) handler->OnKeyDown('C');else handler->OnKeyDown('c'); break;
+         case 0x2F: if(Shift) handler->OnKeyDown('V');else handler->OnKeyDown('v'); break;
+         case 0x30: if(Shift) handler->OnKeyDown('B');else handler->OnKeyDown('b'); break;
+         case 0x31: if(Shift) handler->OnKeyDown('N');else handler->OnKeyDown('n'); break;
+         case 0x32: if(Shift) handler->OnKeyDown('M');else handler->OnKeyDown('m'); break;
+         case 0x33: handler->OnKeyDown(','); break;
+         case 0x34: handler->OnKeyDown('.'); break;
+         case 0x35: handler->OnKeyDown('-'); break;
+
+         case 0x1C: handler->OnKeyDown('\n'); break;
+         case 0x39: handler->OnKeyDown(' '); break;
+         case 0x0f: handler->OnKeyDown('\t');break;  //it handeled in kernel.cpp
+         case 0x2A: case 0x36 :if(Shift) Shift =  false; else Shift = true;break;
 
             default:
             {
